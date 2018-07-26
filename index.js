05e06007bf457c9103e634ade83c1d434dff531a
Charlie Saunders <charlieasaunders@gmail.com>
2018-07-26 09:16:29 -0400

    Fixes start script. Adds error handler and engine specification.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const app = require('koa')();
+const koa = require('koa');
+
+const app = new koa();
 
 const port = process.env.PORT || 8080;
+
+app.on('error', error => {
+    metrics.increment('request.errorCount');
+    log(error);
+});
 
 app.listen(port, () => {
     console.log(JSON.stringify(process.env))

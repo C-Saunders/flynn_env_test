a78bb6dc2f839af2530678a71e91b3bcbb079596
Charlie Saunders <charlieasaunders@gmail.com>
2018-11-17 11:30:38 -0500

    Removes unneeded and broken code.

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 const koa = require('koa');
-
 const app = new koa();
-
 const port = process.env.PORT || 8080;
 
-app.on('error', error => {
-    metrics.increment('request.errorCount');
-    log(error);
-});
-
 app.listen(port, () => {
-    console.log(JSON.stringify(process.env))
+    console.log(JSON.stringify(process.env));
 });
